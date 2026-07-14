7056586c93491e7bdabcc15fb6f6b1aa9e45c8e0
Jatin Singh <70967350+beatenstratblues@users.noreply.github.com>
2026-07-14 22:21:21 +0530

    DEV: Congif internal pkg added

---
+++ pkg/random/generator.go
@@ -0,0 +1,16 @@
+package random
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+func Generator(length int) (string, error) {
+	bytes := make([]byte, length/2)
+
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
+}
