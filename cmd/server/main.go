74aaa3bd3da9151e32c34b49d16283bb20f2c672
Jatin <jatin.singh@brevistay.com>
2026-07-15 14:54:05 +0530

    DEV: relevant pgx package added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/beatenstratblues/maple-workbench/internals/config"
 )
 
 func main() {
+	conf, err := config.Load()
+	if err!=nil {
+		fmt.Println("There is a configuration error", err)
+	}
+	fmt.Println(conf)
 	fmt.Println("Welcome to the mongoDB workbench!")
 }
